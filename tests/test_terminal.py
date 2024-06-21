d4055e20c0fc5a426985e1eaf42e073564bbaa03
Sofia Denner <sofi.denner@gmail.com>
2024-06-21 12:57:50 -0300

    fix tests

--- tests/test_terminal.py
+++ tests/test_terminal.py
@@ -19,7 +19,7 @@ class BaseTestTerminal(unittest.TestCase):
         if 'exit' not in inputs:
             # prevent never ending loop
             inputs = inputs + ['exit']  # avoiding modify received inputs
-        with patch('lamedh.terminal.prompt', side_effect=inputs):
+        with patch('lamedh.terminal.session.prompt', side_effect=inputs):
             self.terminal.main()
         return stdout
 
